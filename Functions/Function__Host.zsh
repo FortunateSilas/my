6c8707fef71d52ab4516c651785ba6d7db6055f0
fortunatesilas <fortunatesilas.art@gmail.com>
2026-04-05 12:28:31 +0200

    Refactor Host function and update sourced scripts in SCRIPTS_APPS.zsh

--- Functions/Function__Host.zsh
+++ Functions/Function__Host.zsh
@@ -1,67 +1,75 @@
 function Host() {
 
+
+	if [[ -z ${1} ]]; then
+
+		cd "${HOME}/Host"
+		echo "Welcome to your Host."
+
+	else
     
+		# Selector
+		local OPTION=${1}
 
-	# Selector
-    local OPTION=${1}
+		case "${OPTION}" in
+		
+			"Init" )
+			# Host Initialize
 
-    case "${OPTION}" in
-		"Init" )
-        # Host Initialize
+				# Comment
+				SOURCEFILE ${DATA_H} "host__initialize.zsh"
 
-			# Comment
-			SOURCEFILE ${DATA_H} "Initialize"
+			;;
+			"Refresh" )
+			# Host Refresh
 
-		;;
-		"Refresh" )
-        # Host Refresh
+				# Comment
+				SOURCEFILE ${DATA_H} "Refresh"
 
-			# Comment
-			SOURCEFILE ${DATA_H} "Refresh"
+			;;
+			"Start" )
+			# Host Start
 
-		;;
-		"Start" )
-        # Host Start
+				# Comment
+				SOURCEFILE ${DATA_H} "Start"
 
-			# Comment
-			SOURCEFILE ${DATA_H} "Start"
+			;;
+			"Stop" )
+			# Host Stop
 
-		;;
-		"Stop" )
-        # Host Stop
+				# Comment
+				SOURCEFILE ${DATA_H} "Stop"
 
-			# Comment
-			SOURCEFILE ${DATA_H} "Stop"
+			;;
+			"Domain" )
+			# Host Stop
 
-		;;
-		"Domain" )
-        # Host Stop
+				# Comment
+				valet_domain ${2}
 
-			# Comment
-			valet_domain ${2}
+			;;
+			"Remove" )
+			# Host Stop
 
-		;;
-		"Remove" )
-        # Host Stop
+				# Remove domain ssl
+				valet_unsecure
 
-			# Remove domain ssl
-			valet_unsecure
+				# Remove current directory from valet's list of paths and domains
+				valet_forget
 
-			# Remove current directory from valet's list of paths and domains
-			valet_forget
+			;;
+			* )
 
-		;;
-		* )
+				echo "You've entered the wrong option"
+				echo "Try one of the following options:"
+				echo "Init"
+				echo "Refresh"
+				echo "Start"
+				echo "Stop"
+				echo "Domain"
 
-			echo "You've entered the wrong option"
-			echo "Try one of the following options:"
-			echo "Init"
-			echo "Refresh"
-			echo "Start"
-			echo "Stop"
-			echo "Domain"
-
-		;;
-	esac
+			;;
+		esac
+	fi
 
 }
--- __/SETTINGS/SCRIPTS_APPS.zsh
+++ __/SETTINGS/SCRIPTS_APPS.zsh
@@ -1,5 +1,6 @@
 source "/home/fortunate/my/__/DATA/Apps/Findmybiz.zsh"
 source "/home/fortunate/my/__/DATA/Apps/Indgnxt.zsh"
 source "/home/fortunate/my/__/DATA/Apps/Starterbricks.zsh"
+source "/home/fortunate/my/__/DATA/Apps/Test01.zsh"
 source "/home/fortunate/my/__/DATA/Apps/Test02.zsh"
 source "/home/fortunate/my/__/DATA/Apps/Test03.zsh"
